0408d7e501fce827654ee1acb29ca576a11bb2f9
sanjay-018-code <sandi115735p@gmail.com>
2026-06-23 17:44:25 +0530

    Fix product links in HomePage component

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -8,9 +8,9 @@ import FeaturedProducts from './FeaturedProducts';
 const HomePage = () => {
   return (
     <div>
-      <HeroSection title="Buy iPone 14 Pro" subtitle="Experience the power of the latest iPhone 14 with our most Pro Camera ever." link='products/6a33a1ace66b5694a9d468f4' image={iphone} />
+      <HeroSection title="Buy iPone 14 Pro" subtitle="Experience the power of the latest iPhone 14 with our most Pro Camera ever." link='product/6a33a1ace66b5694a9d468f4' image={iphone} />
       <FeaturedProducts/>
-      <HeroSection title="Build the ultimate setup" subtitle="You can add Studio Display and colour matched magin accessoris to your bag after configure your Mac mini." link='products/6a33a1ace66b5694a9d468fc' image={mac} />
+      <HeroSection title="Build the ultimate setup" subtitle="You can add Studio Display and colour matched magin accessoris to your bag after configure your Mac mini." link='product/6a33a1ace66b5694a9d468fc' image={mac} />
     </div>
   )
 }
